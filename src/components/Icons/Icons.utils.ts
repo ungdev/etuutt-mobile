b34bb325e9e686a3a294cc02f43288eab1f941f9
Arnaud Dufour <arnaudd@bam.tech>
2020-10-30 22:37:22 +0100

    :sparkles: (setup icons)

---
+++ src/components/Icons/Icons.utils.ts
@@ -0,0 +1,8 @@
+import { IconProps } from './Icons.interface';
+
+export const getSvgProps = (props: IconProps) => ({
+  style: { width: props.size, height: props.size },
+  width: props.size,
+  height: props.size,
+  fill: 'none',
+});
