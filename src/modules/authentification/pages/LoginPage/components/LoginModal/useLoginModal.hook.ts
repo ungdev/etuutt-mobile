94233fb69c00a27a03bfb5fe67d546981ed079cf
quentltl <quentin.letellier@utt.fr>
2021-04-05 16:01:44 +0200

    add api service

--- src/modules/authentification/pages/LoginPage/components/LoginModal/useLoginModal.hook.ts
+++ src/modules/authentification/pages/LoginPage/components/LoginModal/useLoginModal.hook.ts
@@ -2,7 +2,7 @@ import Constants from 'expo-constants';
 import { useRef, useState } from 'react';
 import WebView from 'react-native-webview';
 import { WebViewNavigationEvent } from 'react-native-webview/lib/WebViewTypes';
-import config from '../../../../../../services/api/config';
+import config from '../../../../../api/config';
 
 export const useLoginModal = (onCloseModal: (url?: string) => void) => {
   const baseUri = `https://etu.utt.fr/api/oauth/client-create?name=${config.etu_utt_app_name}&device=${Constants.deviceName}&device_uid=${Constants.deviceId}&scope=${config.etu_utt_scope}`;
--- src/modules/authentification/pages/LoginPage/useLogin.hook.ts
+++ src/modules/authentification/pages/LoginPage/useLogin.hook.ts
@@ -1,15 +1,8 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import axios from 'axios';
-import moment from 'moment';
 import { useContext, useEffect, useState } from 'react';
-import config from '../../../../services/api/config';
-import {
-  ACCESS_TOKEN_EXPIRATION_KEY,
-  ACCESS_TOKEN_KEY,
-  CLIENT_ID_KEY,
-  CLIENT_SECRET_KEY,
-} from '../../../../services/stockage/StorageKey';
+import { CLIENT_ID_KEY, CLIENT_SECRET_KEY } from '../../../../services/stockage/StorageKey';
 import { AuthentificationContext } from '../../context/authentification.context';
+import { getToken } from '../../services/authentification.service';
 
 export const useLogin = () => {
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
@@ -51,38 +44,6 @@ export const useLogin = () => {
       autoLogin();
     } catch (event) {
       console.log(event);
-    }
-  };
-
-  const api = axios.create({
-    baseURL: config.etu_utt_baseuri + '/api/',
-  });
-
-  const getToken = async () => {
-    const expiration_date = await AsyncStorage.getItem(ACCESS_TOKEN_EXPIRATION_KEY);
-    if (expiration_date !== null && moment().isBefore(expiration_date * 1000)) {
-      const token = await AsyncStorage.getItem(ACCESS_TOKEN_KEY);
-      return token;
-    } else {
-      return renewAccessToken();
-    }
-  };
-
-  const renewAccessToken = async () => {
-    try {
-      const clientId = await AsyncStorage.getItem(CLIENT_ID_KEY);
-      const clientSecret = await AsyncStorage.getItem(CLIENT_SECRET_KEY);
-      if (!clientId || !clientSecret) return null;
-      const res = await api.post(
-        `oauth/token?grant_type=client_credentials&scope=${config.etu_utt_scope}&client_id=${clientId}&client_secret=${clientSecret}`
-      );
-
-      await AsyncStorage.setItem(ACCESS_TOKEN_KEY, res.data.access_token);
-      await AsyncStorage.setItem(ACCESS_TOKEN_EXPIRATION_KEY, res.data.expires);
-      return res.data.access_token;
-    } catch (event) {
-      console.log(event);
-      throw 'NO_TOKEN';
     }
   };
 
